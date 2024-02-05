da39feb2888748a2b7af85a264e76423df8b17f3
eduardopaperini <epaperini@gmail.com>
2024-02-05 17:21:16 +0000

    Challenge updated

--- Challenge1.sql
+++ Challenge1.sql
@@ -3,3 +3,9 @@
 Challenge 1
 
 Task: Write a SQL query to select the orders_id, csat_product, and transporter from the table.
+SELECT
+    orders_id
+    , csat_product
+    , transporter
+
+ FROM `applied-buckeye-411311.course14.gwz_nps` 
