a095807015a29ed6da6c10ab52c1a59fb1415d94
Lewis Fraser <156583368+LewisWFraser@users.noreply.github.com>
2024-02-05 17:24:18 +0000

    challenge1
    SQL query for challenge 1

--- Challenge2.sql
+++ Challenge2.sql
@@ -3,3 +3,8 @@
 Challenge 2
 
 Task: Write a SQL query to find all records where the global_note is greater than 7. This challenge introduces the concept of filtering data based on specific conditions.
+
+SELECT *
+FROM `data-analytics-bootcamp-363212.course14.gwz_nps`
+WHERE global_note > 7 
+ORDER BY orders_id
